b66e6c4fdd1a8547b15e94765206c7ff81d902c4
ali <ali@swishtag.com>
2026-06-12 17:31:34 +0500

    update guess

--- resources/js/Pages/Auth/ConfirmPassword.jsx
+++ resources/js/Pages/Auth/ConfirmPassword.jsx
@@ -2,7 +2,7 @@ import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-// import GuestLayout from '@/Layouts/GuestLayout';
+import GuestLayout from '@/Layouts/GuestLayout';
 import { Head, useForm } from '@inertiajs/react';
 
 export default function ConfirmPassword() {
@@ -19,8 +19,8 @@ export default function ConfirmPassword() {
     };
 
     return (
-        <>
-                    <Head title="Confirm Password" />
+        <GuestLayout>
+            <Head title="Confirm Password" />
 
             <div className="mb-4 text-sm text-gray-600">
                 This is a secure area of the application. Please confirm your
@@ -50,7 +50,6 @@ export default function ConfirmPassword() {
                     </PrimaryButton>
                 </div>
             </form>
-        </>
-
+        </GuestLayout>
     );
 }
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -2,6 +2,7 @@ import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
+import GuestLayout from '@/Layouts/GuestLayout';
 import { Head, Link, useForm } from '@inertiajs/react';
 
 export default function Register() {
@@ -21,7 +22,7 @@ export default function Register() {
     };
 
     return (
-        <>
+        <GuestLayout>
             <Head title="Register" />
 
             <form onSubmit={submit}>
@@ -114,6 +115,6 @@ export default function Register() {
                     </PrimaryButton>
                 </div>
             </form>
-        </>
+        </GuestLayout>
     );
 }
